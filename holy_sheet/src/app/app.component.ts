2bc9b303589db9e4a78e50f3abab013f70ef6d22
Fiboliriodem <felipe123654_vcardoso@outlook.com>
2024-06-09 00:25:26 -0300

    implementação de aba

--- holy_sheet/src/app/app.component.ts
+++ holy_sheet/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { ListaFichasComponent } from "./lista-fichas/lista-fichas.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent]
+    imports: [RouterOutlet, HeaderComponent, ListaFichasComponent]
 })
 export class AppComponent {
   title = 'holy_sheet';
---
+++ holy_sheet/src/app/lista-fichas/lista-fichas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-fichas',
+  standalone: true,
+  imports: [],
+  templateUrl: './lista-fichas.component.html',
+  styleUrl: './lista-fichas.component.css'
+})
+export class ListaFichasComponent {
+
+}
