7ed42002765c57106b8ce03cd67811a17233df84
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2024-06-15 19:19:38 -0300

    Adicionando o criador component CSS,HTML E TS

--- holy_sheet/src/app/criador/criador.component.ts
+++ holy_sheet/src/app/criador/criador.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-criador',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './criador.component.html',
-  styleUrl: './criador.component.css'
+  styleUrls: ['./criador.component.css']
 })
 export class CriadorComponent {
+  characterName: string = '';
+  characterRace: string = '';
+  characterClass: string = '';
+  characterLevel: number | null = null;
 
+  strength: number | null = null;
+  dexterity: number | null = null;
+  constitution: number | null = null;
+  intelligence: number | null = null;
+  wisdom: number | null = null;
+  charisma: number | null = null;
+
+  criarFicha() {
+    console.log('Ficha criada:', {
+      characterName: this.characterName,
+      characterRace: this.characterRace,
+      characterClass: this.characterClass,
+      characterLevel: this.characterLevel,
+      strength: this.strength,
+      dexterity: this.dexterity,
+      constitution: this.constitution,
+      intelligence: this.intelligence,
+      wisdom: this.wisdom,
+      charisma: this.charisma,
+    });
+  }
 }
