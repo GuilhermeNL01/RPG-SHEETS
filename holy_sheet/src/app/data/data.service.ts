2be27cee1d6ae355ac85f560879d9e3d1ef6aef0
Fiboliriodem <felipe123654_vcardoso@outlook.com>
2024-06-17 16:55:41 -0300

    testes de servidor pt 2

--- holy_sheet/src/app/data/data.service.ts
+++ holy_sheet/src/app/data/data.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class DataService {
-  private apiUrl = 'http://localhost:3306/api/data';
+  private apiUrl = 'http://localhost:3000/api/data';
 
   constructor(private http: HttpClient) { }
 
