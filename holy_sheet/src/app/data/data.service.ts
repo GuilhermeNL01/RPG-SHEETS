6d89707694feddd91600936a9331e87d68325c04
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2024-06-19 15:20:35 -0300

    Adicionando a lógica da Lista de Fichas

--- holy_sheet/src/app/data/data.service.ts
+++ holy_sheet/src/app/data/data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { data } from 'jquery';
+//import { data } from 'jquery';
 
 @Injectable({
   providedIn: 'root'
--- holy_sheet/src/app/lista-fichas/lista-fichas.component.ts
+++ holy_sheet/src/app/lista-fichas/lista-fichas.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FichaService } from '../data/ficha.service'; 
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-lista-fichas',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './lista-fichas.component.html',
-  styleUrl: './lista-fichas.component.css'
+  styleUrls: ['./lista-fichas.component.css']
 })
-export class ListaFichasComponent {
+export class ListaFichasComponent implements OnInit {
+  fichas: any[] = [];
 
+  constructor(private fichaService: FichaService) { }
+
+  ngOnInit() {
+    this.fichaService.getData().subscribe({
+      next: data => {
+        this.fichas = data;
+      },
+      error: error => {
+        console.error('Erro ao buscar fichas', error);
+      }
+    });
+  }
 }
